d39b25bb124955615d2429b023c26e9ad1ded7b0
Adityariman1 <adityariman96@gmail.com>
2021-11-11 11:04:02 +0700

    larapus

--- resources/views/layouts/bagian/navbar.blade.php
+++ resources/views/layouts/bagian/navbar.blade.php
@@ -126,10 +126,16 @@
           <i class="fas fa-expand-arrows-alt"></i>
         </a>
       </li>
+
       <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-          <i class="fas fa-th-large"></i>
-        </a>
+        <a class ="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+        document.getElementById('logout-form').submit();" role="button">
+      Logout <i class="fas fa-sign-out-alt"></i>  
+      </a>
+      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+        @csrf
+      </form>
       </li>
+
     </ul>
   </nav>
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,10 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::group(['prefix'=>'admin', 'middleware'=>['auth']], function () {
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['prefix'=>'admin', 'middleware'=>['auth', 'role:Admin']], function () {
+    Route::get('/', function(){
+        return view('admin');
+    });
 });
